6fc3ac803050ffd69c776b216aa957ac9b8456d4
Josh Payne <snare117@gmail.com>
2023-11-04 03:33:42 -0700

    add frontend

---
+++ fe/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-start items-start h-screen">
+      <div className="mb-auto text-left">
+        Hello World
+      </div>
+    </main>
+  )
+}
