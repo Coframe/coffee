fda4cbcabc3cfe541f1e97f1685a77a5f4ff6ce8
Pavlo <pavlo@Pavlos-MacBook-Pro.local>
2023-11-21 00:33:02 -0800

    refactor + error button

--- coffee/extension/content.js
+++ coffee/extension/content.js
@@ -1,176 +1,269 @@
-let currentElement = null;
+(function() {
+  // Define application state and operations
+  const appState = {
+      consoleErrors: [],
+      loading: false,
+      selectionMode: true,
+      currentElement: null,
 
-var div = document.createElement('div');
-div.id = 'coframeCoffeeDiv';
-div.style.position = 'fixed';
-div.style.top = '10px';
-div.style.right = '10px';
-div.style.zIndex = '1000';
-div.style.backgroundColor = 'rgba(10, 10, 10, 0.8)';
-div.style.padding = '10px';
-div.style.borderRadius = '10px';
-div.style.width = '450px';
-div.style.backdropFilter = 'blur(10px)';
-div.style.boxShadow = '10px 10px 10px rgba(0, 0, 0, 0.2)';
-div.style.border = '1px solid rgba(255, 255, 255, 0.1)';
-div.style.cursor = 'move';
+      setConsoleErrors(newErrors) {
+          this.consoleErrors = newErrors;
+          SendErrorsButton().style.display = newErrors.length > 0 ? 'block' : 'none';
+      },
 
-// Add your interface to the div
-div.innerHTML = `
-  <input id="coframeCoffeeInput" type="text" style="width: 100%; padding: 5px; box-sizing: border-box;">
-  <div id='coframeCoffeeMeta' style="display: flex; gap: 8px">
-    <div id="coframeCoffeePreview">Press an element to modify it specifically</div>
-    <button id="coframeCoffeeButton" style="float:right; padding: 5px; height: 38px; min-width: 90px; margin-top: 10px; border: none; border-radius: 5px; cursor: pointer; background-color: #0076FF; color: #fff; text-align: right;">Update ✨</button>
-  </div>
-  <div id="loading" style="display: none; color: white; animation: pulse 1s infinite;">
-  ✨ Making magic<span class="dot">.</span><span class="dot">.</span><span class="dot">.</span>
-  </div>
+      setLoading(loading) {
+          this.loading = loading;
+          LoadingIndicator().style.display = loading ? 'block' : 'none';
+      },
 
-<style>
-@keyframes pulse {
-  0% {
-    opacity: 1;
-  }
-  50% {
-    opacity: 0.7;
-  }
-  100% {
-    opacity: 1;
-  }
-}
+      setSelectionMode(newSelectionMode) {
+          this.selectionMode = newSelectionMode;
+          ToogleSelectionModeButton().style.backgroundColor = newSelectionMode ? '#0076FF' : '#ffffff1a';
+          SelectedDomPreview().style.opacity = newSelectionMode ? 1 : 0.0;
+          this.currentElement && this.setCurrentElement(null);
+          document.body.classList.toggle('selection-mode-active', newSelectionMode);
+      },
 
-.dot {
-  animation: dot 1s infinite;
-}
-
-.dot:nth-child(2) {
-  animation-delay: 0.2s;
-}
-
-.dot:nth-child(3) {
-  animation-delay: 0.4s;
-}
-
-@keyframes dot {
-  0%, 20%, 100% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-}
-
-.coffee-highlighted-class {
-  background-color: rgba(173, 216, 230, 0.5);
-  outline: 1px solid #ADD8E6;
-}
-
-#coframeCoffeePreview {
-  font-size: 12px;
-  color: white;
-  font-family: monospace;
-  margin-top: 10px;
-  padding: 10px;
-  background-color: rgba(255, 255, 255, 0.1);
-  border-radius: 5px;
-  overflow: auto;
-  max-height: 200px;
-}
-</style>
-`;
-
-// Add the div to the body
-document.body.appendChild(div);
-
-// Add an event listener to the button
-document.getElementById('coframeCoffeeButton').addEventListener('click', function() {
-  var inputText = document.getElementById('coframeCoffeeInput').value;
-  
-  // Show the loading animation
-  document.getElementById('loading').style.display = 'block';
-
-  // Send the input text to your server
-  let body = {
-    text: inputText,
+      setCurrentElement(element) {
+          if (this.currentElement) {
+              this.currentElement.classList.remove('coffee-highlighted-class');
+          }
+          this.currentElement = element;
+          const previewText = element ? element.outerHTML : 'Press an element to modify it specifically';
+          SelectedDomPreview().innerText = previewText;
+          element && element.classList.add('coffee-highlighted-class');
+      }
   };
 
-  if (currentElement) {
-    body['html'] = currentElement.outerHTML;
-  }
-  
-  fetch('http://localhost:8000/prompt', {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // Hide the loading animation
-    document.getElementById('loading').style.display = 'none';
-  })
-    .catch((error) => {
-    console.error('Error:', error);
-    // Hide the loading animation
-    document.getElementById('loading').style.display = 'none';
-  });
 
-  // Clear the input field immediately after sending the request
-  document.getElementById('coframeCoffeeInput').value = '';
-});
-
-// Add a keypress event listener to the input field
-document.getElementById('coframeCoffeeInput').addEventListener('keypress', function(e) {
-  if (e.key === 'Enter') {
-    document.getElementById('coframeCoffeeButton').click();
-  }
-});
-
-// Add drag and drop functionality
-var isMouseDown = false;
-var mouseX, mouseY;
-
-div.addEventListener('mousedown', function(e) {
-  if (e.target.id === 'coframeCoffeeInput') {
-    return;
-  }
-  isMouseDown = true;
-  mouseX = e.clientX - div.offsetLeft;
-  mouseY = e.clientY - div.offsetTop;
-});
-
-document.addEventListener('mousemove', function(e) {
-  if (isMouseDown) {
-    div.style.left = (e.clientX - mouseX) + 'px';
-    div.style.top = (e.clientY - mouseY) + 'px';
-  }
-});
-
-document.addEventListener('mouseup', function() {
-  isMouseDown = false;
-});
-
-// Add a click event listener to all elements
-document.body.addEventListener('click', function(e) {
-  if (e.target.closest('#coframeCoffeeDiv')) {
-    return;
+  // Create UI components
+  function createCoffeeUI() {
+      const div = document.createElement('div');
+      div.id = 'coframeCoffeeDiv';
+      div.innerHTML = getCoffeeUiHtml();
+      return div;
   }
 
-  if (currentElement) {
-    currentElement.classList.remove('coffee-highlighted-class');
-    document.getElementById('coframeCoffeePreview').innerText = 'Press an element to modify it specifically';
-    if (e.target === currentElement) {
-      currentElement = null;
-      return;
+  function getCoffeeUiHtml() {
+    // Returns the inner HTML for the coffee UI
+    const loadingDots = Array(3).fill('<span class="CoffeeLoadingDot">.</span>').join('');
+
+    return `
+        <input id="CoffeeInput" type="text">
+        <div id='CoffeeMeta'>
+            <button id='CoffeeToggleSelectionMode'>🎯</button>
+            <div id="CoffeePreview">Press an element to modify it specifically</div>
+            <button id="CoffeeSendErrorsButton">🐞</button>
+            <button id="CoffeeUpdateButton">Update ✨</button>
+        </div>
+        <div id="CoffeeLoadingIndicator">
+            <span class="CoffeePulse">✨</span> Making magic${loadingDots}
+        </div>
+        <style>
+            #coframeCoffeeDiv {
+              position: fixed;
+              top: 10px;
+              right: 10px;
+              z-index: 100000;
+              background: rgba(10, 10, 10, 0.8);
+              padding: 10px;
+              border-radius: 10px;
+              width: 450px;
+              backdrop-filter: blur(10px);
+              box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.2);
+              border: 1px solid rgba(255, 255, 255, 0.1);
+              cursor: move;
+            }
+
+            #CoffeeInput {
+              padding: 5px;
+              border-radius: 5px;
+              box-sizing: border-box;
+              width: 100%;
+              border: none;
+            }
+            #CoffeeMeta {
+                display: flex;
+                gap: 8px;
+            }
+            #CoffeeMeta button {
+                height: 38px;
+                margin-top: 10px;
+                border: none;
+                cursor: pointer;
+                background-color: #2d2929;
+                color: #fff;
+                padding: 5px;
+                border-radius: 5px;
+                box-sizing: border-box;
+            }
+            #CoffeeToggleSelectionMode {
+                background-color: #0076FF;
+            }
+            #CoffeePreview {
+              font-size: 12px;
+              color: white;
+              font-family: monospace;
+              margin-top: 10px;
+              padding: 0 10px;
+              background-color: rgba(255, 255, 255, 0.1);
+              border-radius: 5px;
+              overflow: auto;
+              max-height: fit-content;
+              line-height: 38px;
+              white-space: nowrap;
+            }
+
+            #CoffeeMeta button#CoffeeUpdateButton {
+                float: right;
+                min-width: 90px;
+                background-color: #0076FF;
+                color: #fff;
+                text-align: right;
+            }
+            #CoffeeLoadingIndicator {
+                margin-top: 10px;
+                display: none;
+                color: white;
+            }
+
+            .CoffeeLoadingDot {
+              animation: dot 1s infinite;
+            }
+
+            .CoffeeLoadingDot:nth-child(2) {
+              animation-delay: 0.2s;
+            }
+
+            .CoffeeLoadingDot:nth-child(3) {
+              animation-delay: 0.4s;
+            }
+
+            @keyframes dot {
+              0%, 100% {
+                opacity: 0;
+              }
+              50% {
+                opacity: 1;
+              }
+            }
+
+            .CoffeePulse {
+              animation: pulse 1s infinite;
+            }
+
+            @keyframes pulse {
+              0% {
+                opacity: 1;
+              }
+              50% {
+                opacity: 0.7;
+              }
+              100% {
+                opacity: 1;
+              }
+            }
+        </style>
+    `;
+}
+
+
+  // Fetching and sending data
+  async function postData(url = '', data = {}) {
+    appState.setConsoleErrors([]);
+    appState.setLoading(true);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const json = await response.json();
+        appState.setLoading(false);
+        return json;
+    } catch (error) {
+        console.log('Error generating:', error);
+        appState.setLoading(false);
     }
   }
-  // Get the outer HTML of the hovered element
-  var html = e.target.outerHTML;
-  console.log(html);
-  // highlight the element
-  e.target.classList.add('coffee-highlighted-class');
-  currentElement = e.target;
-  document.getElementById('coframeCoffeePreview').innerText = html;
-});
+
+  async function sendPrompt() {
+    var inputText = PromptInput().value;
+    let body = {
+        text: inputText,
+        html: appState.currentElement ? appState.currentElement.outerHTML : null,
+    };
+
+    try {
+        const data = await postData('http://localhost:8000/prompt', body);
+        console.log('Generation success', data);
+    } catch (error) {
+        // ...
+    }
+
+    PromptInput().value = '';
+  }
+
+  async function handleDomClick(e) {
+    if (!appState.selectionMode || e.target.closest('#coframeCoffeeDiv')) {
+      return;
+    }
+    console.log('handleDomClick')
+    if (e.target === appState.currentElement) {
+      appState.setCurrentElement(null);
+    } else {
+      appState.setCurrentElement(e.target);
+    }
+  }
+
+
+
+  // Error Sending Function
+  async function sendConsoleErrors() {
+      if (appState.consoleErrors.length === 0) {
+          return;
+      }
+
+      try {
+          const data = await postData('http://localhost:8000/errors', appState.consoleErrors.map(e => ({ message: e.args.join('\n') })));
+          console.log('Console Errors sent', data);
+      } catch (error) {
+          // Error handling
+      }
+  }
+
+
+  ToogleSelectionModeButton = () => document.getElementById('CoffeeToggleSelectionMode');
+  SelectedDomPreview = () => document.getElementById('CoffeePreview');
+  SendErrorsButton = () => document.getElementById('CoffeeSendErrorsButton');
+  SendPromptButton = () => document.getElementById('CoffeeUpdateButton');
+  PromptInput = () => document.getElementById('CoffeeInput');
+  LoadingIndicator = () => document.getElementById('CoffeeLoadingIndicator');
+
+  // Event Binding
+  function bindEventListeners() {
+      document.body.addEventListener('click', handleDomClick);
+      SendErrorsButton().addEventListener('click', sendConsoleErrors);
+      ToogleSelectionModeButton().addEventListener('click', () => appState.setSelectionMode(!appState.selectionMode));
+      SendPromptButton().addEventListener('click', sendPrompt);
+      PromptInput().addEventListener('keypress', function(e) {
+          if (e.key === 'Enter') {
+              sendPrompt();
+          }
+      });
+  }
+
+  // Initialize
+  function initialize() {
+      const ui = createCoffeeUI();
+      document.body.appendChild(ui);
+      bindEventListeners();
+      appState.setConsoleErrors([]);
+      appState.setLoading(false);
+      appState.setSelectionMode(true);
+  }
+
+  initialize();
+})();
--- coffee/maker/main.py
+++ coffee/maker/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from langchain.callbacks import get_openai_callback
 from pydantic import BaseModel
 import os
-from typing import Optional
+from typing import Optional, List
 
 from agent.agent_gpt import get_agent
 
@@ -36,9 +36,22 @@ agent = get_agent(os.environ.get("FRONTEND_DIR"))
 @app.post("/prompt")
 async def generate(prompt: Prompt):
     prompt.file = prompt.file or "app/page.tsx"
-    task =  f"User query: {prompt.text}\n\n" \
-            f"Currently user selected this element: {prompt.html}." \
-            f"Currently user is looking at this file: {prompt.file}"
+    task =  f"User request: {prompt.text}\n" \
+            f"Currently user selected this element: {prompt.html}.\n" \
+            f"Currently user is looking at this file: {prompt.file}\n"
+    print(task)
+    with get_openai_callback() as cb:
+        agent.run([task])
+        print(cb)
+    return {"message": "Done"}
+
+class Error(BaseModel):
+    message: str
+
+@app.post("/errors")
+async def handle_errors(errors:List[Error]):
+    error_messages = '\n'.join([e.message for e in errors])
+    task =  f"Browser logged errors: {error_messages}\n. Fix it."
     print(task)
     with get_openai_callback() as cb:
         agent.run([task])
