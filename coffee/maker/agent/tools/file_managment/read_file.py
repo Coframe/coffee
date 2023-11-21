60235ac93c84c8c0fc1b87ab1454a75d4e1d6190
Aleksey Korshuk <48794610+AlekseyKorshuk@users.noreply.github.com>
2023-11-20 16:57:04 -0800

    Update read_file.py

--- coffee/maker/agent/tools/file_managment/read_file.py
+++ coffee/maker/agent/tools/file_managment/read_file.py
@@ -16,32 +16,6 @@ class ReadFileInput(BaseModel):
     file_path: str = Field(..., description="Full path to the file.")
 
 
-# class ReadFileTool(BaseFileToolMixin, BaseTool):
-#     """Tool that reads a file and includes line index (starts from zero) in the output - `{i}->`."""
-#
-#     name: str = "read_file"
-#     args_schema: Type[BaseModel] = ReadFileInput
-#     description: str = "Read a file and include line index (starts from zero) in the output - `{i}->`."
-#
-#     def _run(
-#             self,
-#             file_path: str,
-#             run_manager: Optional[CallbackManagerForToolRun] = None,
-#     ) -> str:
-#         try:
-#             read_path = self.get_relative_path(file_path)
-#         except FileValidationError:
-#             return INVALID_PATH_TEMPLATE.format(arg_name="file_path", value=file_path)
-#         if not read_path.exists():
-#             return f"Error: no such file or directory: {file_path}"
-#         try:
-#             with read_path.open("r", encoding="utf-8") as f:
-#                 content = ''.join([f"{i}->{line}" for i, line in enumerate(f)])
-#
-#             return content
-#         except Exception as e:
-#             return "Error: " + str(e)
-
 class ReadFileTool(BaseFileToolMixin, BaseTool):
     """Tool that reads a file."""
 
