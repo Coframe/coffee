cb66b0a72745b9a9e6589a687e3c991296a14c50
Pavlo <pavlo@Pavlos-MacBook-Pro.local>
2023-12-04 22:16:07 -0800

    upd

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,13 +3,19 @@ import { useState, useEffect } from 'react';
 import Coffee from './coffee/Coffee';
 
 export default function Page() {
-  const [operations, setOperations] = useState('1+1');
-  console.log(operations);
+  const [operationsScreen, setOperationsScreen] = useState('1+1');
+  const performOperations = () => {
+    const result = eval(operationsScreen);
+    console.log(`performed operations in parent component: ${operationsScreen} = ${result}`);
+    setOperationsScreen(result);
+  }
+
   return (
     <>
-      <Coffee brew="Calculator.tsx" operations={operations} setOperations={setOperations}>
+      <Coffee brew="Calculator.tsx" operationsScreen={operationsScreen} setOperationsScreen={setOperationsScreen} performOperations={performOperations}>
         Generate Calculator UI using tailwind css.
         Don't use col-span, all buttons same size.
+        All calculations are done in the parent component.
       </Coffee>
     </>
   )
