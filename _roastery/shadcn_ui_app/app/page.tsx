245836baea633f8cc7407c043486d2d27fd9d53b
Pavlo Razumovskyi <onum.mail@gmail.com>
2023-12-11 14:28:15 -0800

    Add shadcn example app (#35)
    * add shadcn example app
    
    * add one more component
    
    ---------
    
    Co-authored-by: Pavlo <pavlo@Pavlos-MacBook-Pro.local>

---
+++ _roastery/shadcn_ui_app/app/page.tsx
@@ -0,0 +1,32 @@
+import Nav from './nav'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      </main>
+    </>
+  )
+}
