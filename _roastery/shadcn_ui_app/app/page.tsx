874fd932b5cc81ca9c45c17692d61abe5cd21182
Pavlo <pavlo@Pavlos-MacBook-Pro.local>
2023-12-11 14:03:05 -0800

    add shadcn example app

---
+++ _roastery/shadcn_ui_app/app/page.tsx
@@ -0,0 +1,12 @@
+import Nav from './nav'
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Hello world
+      </main>
+    </>
+  )
+}
